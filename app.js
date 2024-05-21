69a6306a9d59eb2c2e5596f0bf96327adb73825f
Omadbek571 <omadbektomsk08@gmail.com>
2024-05-22 00:55:30 +0500

    .

---
+++ app.js
@@ -0,0 +1,205 @@
+// 1-savol
+// let son = +prompt("Iltimos son kiriting");
+
+// if (son > 0) {
+//     console.log("musbat");
+// } else {
+//     console.log("manfiy");
+// }
+
+// 2-savol
+
+// let num1 = +prompt("Iltimos son kiriting");
+
+// if(num1 % 2 === 1){
+//     console.log("Toq son");
+// }else{
+//     console.log("JUft son");
+
+// }
+
+// 3-savol
+
+
+// let num = +prompt("Iltimos son kiriting");
+
+// if (num % 2 === 0) {
+//     console.log("Bu son juft son");
+// } else {
+//     console.log("Bu son toq son");
+// }
+
+
+// 4-savol
+
+
+// let A = 8;
+// let B = 8;
+
+// if (A > 2) {
+//     if (B <= 3) {
+//         console.log("Togri");
+//     } else {
+//         console.log("Yolgon");
+//     }
+// } else {
+//     console.log("Rostlka javob bermidi");
+// }
+
+
+// 5-savol
+
+
+// let num1 = +prompt("Iltimos A son kiriting");
+// let num2 = +prompt("Iltimos B son kiriting");
+// let natija = "Notogri"
+
+
+// if (num1 >= 0) {
+//     natija = "Togri";
+// } else {
+//     console.log("2");
+//     if(num2 < -2){
+//         natija = "togri";
+//     }
+// }
+
+// console.log(natija);
+
+// 6-savol
+
+// let A = 4;
+// let B = 4;
+// let C = 4;
+
+// let natija = A <= B && B <= C;
+
+
+// console.log(natija);
+
+// 7-savol
+
+// let A = 8;
+// let B = 7;
+// let C = 2;
+
+// if (B > A && B < C) {
+//     console.log("Truee");
+// } else {
+//     console.log("Falsee");
+
+// }
+
+// 8-savol
+
+// let A = 3;
+// let B = 5;
+
+// if (A % 2 !== 0 && B % 2 !== 0) {
+//     console.log("Toq son"); 
+// } else {
+//     console.log("Toq emas"); 
+// }
+
+
+// 9-savol
+
+// let A = 3;
+// let B = 6;
+
+// if ((A % 2 !== 0 || B % 2 !== 0) && (A % 2 === 0 || B % 2 === 0)) {
+//     console.log("Toq son bor"); 
+// } else {
+//     console.log("Bittasiham toq emas");
+// }
+
+
+// 10-savol
+
+
+// let A = 3;
+// let B = 6;
+
+// if ((A % 2 !== 0 && B % 2 === 0) || (A % 2 === 0 && B % 2 !== 0)) {
+//     console.log("A va B sonlarning faqat bittasi toq son"); 
+// } else {
+//     console.log("A va B sonlarning faqat bittasi toq son emas"); 
+// }
+
+
+// 11-savol
+
+// let A = 3;
+// let B = 6;
+
+// if ((A % 2 === 0 && B % 2 === 0) || (A % 2 !== 0 && B % 2 !== 0)) {
+//     console.log("A va B sonlarining har ikkalasi ham yoki toq son yoki juft son"); 
+// } else {
+//     console.log("A va B sonlarining har ikkalasi ham biri toq, biri juft son emas"); 
+// }
+
+
+// 12-savol
+
+// let A = 3;
+// let B = -6;
+// let C = 8;
+
+// if (A > 0 && B > 0 && C > 0) {
+//     console.log("A, B, C sonlarning har biri musbat"); 
+// } else {
+//     console.log("A, B, C sonlarning ichida kamida bittasi manfiy son"); 
+// }
+
+// 13-savol
+
+
+// let A =  +prompt("Iltimos A sonini kiriting");
+// let B =  +prompt("Iltimos B sonini kiriting");
+// let C = +prompt("Iltimos C sonini kiriting");
+
+// if (A > 0 || B > 0 || C > 0) {
+//     console.log("A, B, C bittasi musbat"); 
+// } else {
+//     console.log("A, B, C  bittasi manfiy"); 
+// }
+
+
+// 14-savol
+
+// let A = -3;
+// let B = -6;
+// let C = 8;
+
+// if ((A > 0 && B <= 0 && C <= 0) || (A <= 0 && B > 0 && C <= 0) || (A <= 0 && B <= 0 && C > 0)) {
+//     console.log("A, B, C sonlaridan faqat bittasi musbat son"); 
+// } else {
+//     console.log("A, B, C sonlarning hammasi yoki hech biri musbat son emas"); 
+// }
+
+
+// 15-savol
+
+
+// let A = 3;
+// let B = -6;
+// let C = 8;
+
+// if ((A > 0 && B > 0 && C <= 0) || (A > 0 && B <= 0 && C > 0) || (A <= 0 && B > 0 && C > 0)) {
+//     console.log("A, B, C sonlardan faqat ikkitasi musbat son"); 
+// } else {
+//     console.log("A, B, C sonlarning hammasi yoki hech biri musbat son emas"); 
+// }
+
+
+// 16-savol
+
+// let son = 42;
+
+// if (son > 9 && son < 100 && son % 2 === 0) {
+//     console.log("Berilgan son ikki xonali juft son");
+// } else {
+//     console.log("Berilgan son ikki xonali juft son emas yoki manfiy son"); 
+// }
+
+
